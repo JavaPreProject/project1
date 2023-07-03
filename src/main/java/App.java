de44412c4c5a904252c15fd812820e970a6cc145
MaximButmanov <butmanovmax@gmail.com>
2023-07-03 12:03:43 +0300

    task 2.1.3 is done

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+
+        System.out.println(bean.equals(bean1));
+        System.out.println(beanCat.equals(beanCat2));
+    }
+}
