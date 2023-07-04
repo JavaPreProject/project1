c1f3bb9adbaafe67cad7ee33e5cfebc373301fb1
MaximButmanov <butmanovmax@gmail.com>
2023-07-04 12:24:00 +0300

    Task 2.1.4 is done

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,15 @@
+package app.config;
+
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+    @Bean
+    public Dog myClass(){
+        return new Dog();
+    }
+}
